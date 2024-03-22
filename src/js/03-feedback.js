22531fe994b28c42d3b86db02a3a10b9c0925421
Ayten <139014081+Bonefletcher24@users.noreply.github.com>
2024-03-22 13:36:21 +0400

    upd3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,52 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('form.feedback-form')
+const emailEL = document.querySelector('label [name="email"]');
+const messageEL = document.querySelector('label [name="message"]');
+
+const STORAGE_KEY = 'feedback-form-state';
+
+form.addEventListener('input', throttle(onFormInput, 500));
+
+function onFormInput() {
+    const email = emailEL.value;
+    const message = messageEL.value;
+    
+    const formData = {
+        email,
+        message,
+    };
+
+localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+onPageReload();
+
+function onPageReload() {
+    const savedMessage = JSON.parse(localStorage.getItem(STORAGE_KEY));
+    if(savedMessage) {
+        emailEL.value = savedMessage.email;
+        messageEL.value = savedMessage.message;  
+    }
+}
+
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(e) {
+    e.preventDefault(); //reloading the page
+    const email = emailEL.value;
+    const message = messageEL.value;
+
+    if (email == '' || message == '') {
+        alert('Enter both input parameters!');
+        form.reset();
+        return;
+    }
+
+    const formData = { email, message };
+    console.log(formData);
+    form.reset();
+    
+
+    localStorage.removeItem(STORAGE_KEY);
+}
